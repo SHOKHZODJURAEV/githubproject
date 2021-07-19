0e85895d11f4b41b3fd1cbe57608def2f400a67a
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-07-19 18:19:45 -0400

    let us do it

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello git World");
+        System.out.println("Hello guys one again");
 
     }
-
-
 }
